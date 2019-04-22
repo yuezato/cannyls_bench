f720df4edf2efe22d0c9194bfadd2fc33cc3e5d6
yuuya uezato <yuuya_uezato@dwango.co.jp>
2019-04-23 01:26:20 +0900

    call run_side_job_once to release portions

--- src/storage.rs
+++ src/storage.rs
@@ -20,18 +20,22 @@ pub fn create_storage<P: AsRef<Path>>(
 /// PUTした直後にDELETEを行う。
 pub fn bench1<P: AsRef<Path>>(path: P, journal_size: u64, data_region_size: u64) {
     let storage = create_storage(path, journal_size, data_region_size).unwrap();
+    println!("{:?}", storage.header());
     bench1_inner(storage, super::man!(10), 512);
 }
 
 fn bench1_inner(mut storage: Storage<FileNvm>, iter: usize, data_size: usize) {
     let _timer = timer::Timer::new(&format!("bench1 with {:?} and {:?}", iter, data_size));
-
+    let lump_data = storage
+        .allocate_lump_data_with_bytes(vec![42; data_size].as_slice())
+        .unwrap();
     for i in 0..iter {
         let lump_id = LumpId::new(i as u128);
-        let lump_data = storage
-            .allocate_lump_data_with_bytes(vec![(i % 255) as u8; data_size].as_slice())
-            .unwrap();
         storage.put(&lump_id, &lump_data).unwrap();
         storage.delete(&lump_id).unwrap();
+
+        if i % 100 == 0 {
+            storage.run_side_job_once().unwrap();
+        }
     }
 }
